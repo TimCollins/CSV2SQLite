b080bf5de21e8568795b354c13f5d3adc4767f71
Tim <tim@timcollins.ie>
2018-01-08 18:14:16 +0000

    Added unit test project.

---
+++ CSV2SQLite.UnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CSV2SQLite.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Something()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
