6140495393014c78ccc61a7ea793658c946b56dc
Tim <tim@timcollins.ie>
2018-01-17 21:18:48 +0000

    Added tests class.

---
+++ src/CSV2SQLite.UnitTests/GeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using CSV2SQLite.App;
+using CSV2SQLite.App.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace CSV2SQLite.UnitTests
+{
+    [TestFixture]
+    public class GeneratorTests
+    {
+        private SQLiteGenerator _generator;
+        private Mock<IFileWrapper> _fileWrapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileWrapper = new Mock<IFileWrapper>();
+            _fileWrapper.Setup(m => m.Exists(It.IsAny<string>())).Returns(true);
+            _generator = new SQLiteGenerator(_fileWrapper.Object);
+        }
+    }
+}
