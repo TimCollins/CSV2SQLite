65c17a5d7a1f745495b320943d06e28ddc5b19eb
Tim <tim@timcollins.ie>
2018-01-21 18:21:30 +0000

    Added test for CsvHeaderException.

--- src/CSV2SQLite.UnitTests/GeneratorTests.cs
+++ src/CSV2SQLite.UnitTests/GeneratorTests.cs
@@ -1,4 +1,7 @@
-﻿using CSV2SQLite.App;
+﻿using System.IO;
+using System.Text;
+using CSV2SQLite.App;
+using CSV2SQLite.App.Exceptions;
 using CSV2SQLite.App.Interfaces;
 using Moq;
 using NUnit.Framework;
@@ -17,6 +20,22 @@ namespace CSV2SQLite.UnitTests
             _fileWrapper = new Mock<IFileWrapper>();
             _fileWrapper.Setup(m => m.Exists(It.IsAny<string>())).Returns(true);
             _generator = new SQLiteGenerator(_fileWrapper.Object);
+        }
+
+        [Test]
+        public void CsvHeaderExceptionWillBeThrownForEmptyFile()
+        {
+            var stream  = new MemoryStream(Encoding.UTF8.GetBytes(string.Empty));
+            var streamReader = new StreamReader(stream);
+            _fileWrapper.Setup(m => m.Open(It.IsAny<string>())).Returns(streamReader);
+
+            var config = new Configuration
+            {
+                InputFilename = "input.csv",
+                OutputFilename = "output.sql"
+            };
+
+            Assert.Throws<CsvHeaderException>(() => _generator.Generate(config));
         }
     }
 }
