8536a9f8494ed883b9d5accc5272735c9581ac4a
Tim <tim@timcollins.ie>
2018-01-21 17:21:12 +0000

    Added batch files to generate coverage.

---
+++ src/CSV2SQLite.UnitTests/run-coverage.bat
@@ -0,0 +1,1 @@
+..\..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:run-tests.bat -register:user -filter:+[csv2sqlite]*
---
+++ src/CSV2SQLite.UnitTests/run-report.bat
@@ -0,0 +1,1 @@
+..\..\..\..\packages\ReportGenerator.3.1.1\tools\reportgenerator.exe -reports:results.xml -targetdir:coverage
